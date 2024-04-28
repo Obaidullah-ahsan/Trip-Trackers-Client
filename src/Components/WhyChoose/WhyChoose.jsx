c56b89850077b190612480854a4f8c6a723d6c66
Obaidullah-ahsan <abir60303@gmail.com>
2024-04-28 10:36:26 +0600

    why choose us section done

--- src/Components/WhyChoose/WhyChoose.jsx
+++ src/Components/WhyChoose/WhyChoose.jsx
@@ -1,13 +1,12 @@
 import { Fade } from "react-awesome-reveal";
-import { Navigation, Pagination } from "swiper/modules";
+import { Autoplay, Navigation} from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 const WhyChoose = () => {
   return (
-    <div className="my-20">
+    <div className="my-28">
       <div className="max-w-4xl ml-20">
         <h3 className="text-[#fc8621] text-lg font-bold">WHAT WE SERVE</h3>
         <Fade cascade Fade direction="up">
@@ -29,113 +28,110 @@ const WhyChoose = () => {
       <Fade Fade direction="up">
         <div className="mx-20">
           <Swiper
-            slidesPerView={3}
+            lg:slidesPerView={3}
             spaceBetween={30}
-            navigation={true}
-            pagination={{
-              clickable: true,
+            autoplay={{
+              delay: 2000,
+              disableOnInteraction: false,
             }}
+            navigation={true}
             loop={true}
-            modules={[Pagination, Navigation]}
+            modules={[Autoplay, Navigation]}
             className="mySwiper"
           >
             <SwiperSlide>
-              <div className="rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800">
-                <img
-                  src="https://source.unsplash.com/random/300x300/?2"
-                  alt=""
-                  className="object-cover object-center w-full rounded-t-md h-44 dark:bg-gray-500"
-                />
-                <div className="flex flex-col justify-between p-6 space-y-8">
-                  <div className="space-y-2">
-                    <h2 className="text-3xl font-semibold tracking-wide">
-                      Donec lectus leo
-                    </h2>
-                    <p className="dark:text-gray-800">
-                      Curabitur luctus erat nunc, sed ullamcorper erat
-                      vestibulum eget.
-                    </p>
-                  </div>
+              <div className="card rounded-sm card-compact bg-base-200">
+                <figure>
+                  <img
+                    src="https://i.ibb.co/hZydnMF/famous-eiffel-tower-paris-with-gorgeous-colors.jpg"
+                    alt="Shoes"
+                    className="h-44 w-full"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">Enjoy the beauty of Paris</h2>
+                  <p>
+                    Famous eiffel tower paris with gorgeous colors and beauty of
+                    paris
+                  </p>
                 </div>
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className=" rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800">
-                <img
-                  src="https://source.unsplash.com/random/300x300/?2"
-                  alt=""
-                  className="object-cover object-center w-full rounded-t-md h-44 dark:bg-gray-500"
-                />
-                <div className="flex flex-col justify-between p-6 space-y-8">
-                  <div className="space-y-2">
-                    <h2 className="text-3xl font-semibold tracking-wide">
-                      Donec lectus leo
-                    </h2>
-                    <p className="dark:text-gray-800">
-                      Curabitur luctus erat nunc, sed ullamcorper erat
-                      vestibulum eget.
-                    </p>
-                  </div>
+              <div className="card rounded-sm card-compact bg-base-200">
+                <figure>
+                  <img
+                    src="https://i.ibb.co/722NYFh/19667.jpg"
+                    alt="Shoes"
+                    className="h-44 w-full"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">Enjoy the beauty of Italy</h2>
+                  <p>
+                    Cinque Terre National Park surrounded by the sea under the
+                    sunlight in Italy
+                  </p>
                 </div>
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className=" rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800">
-                <img
-                  src="https://source.unsplash.com/random/300x300/?2"
-                  alt=""
-                  className="object-cover object-center w-full rounded-t-md h-44 dark:bg-gray-500"
-                />
-                <div className="flex flex-col justify-between p-6 space-y-8">
-                  <div className="space-y-2">
-                    <h2 className="text-3xl font-semibold tracking-wide">
-                      Donec lectus leo
-                    </h2>
-                    <p className="dark:text-gray-800">
-                      Curabitur luctus erat nunc, sed ullamcorper erat
-                      vestibulum eget.
-                    </p>
-                  </div>
+              <div className="card rounded-sm card-compact bg-base-200">
+                <figure>
+                  <img
+                    src="https://i.ibb.co/1ZzX7x4/1390.jpg"
+                    alt="Shoes"
+                    className="h-44 w-full"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">Enjoy the beauty of Englend</h2>
+                  <p>Famous Tower Bridge in the evening, London, England</p>
                 </div>
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className=" rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800">
-                <img
-                  src="https://source.unsplash.com/random/300x300/?2"
-                  alt=""
-                  className="object-cover object-center w-full rounded-t-md h-44 dark:bg-gray-500"
-                />
-                <div className="flex flex-col justify-between p-6 space-y-8">
-                  <div className="space-y-2">
-                    <h2 className="text-3xl font-semibold tracking-wide">
-                      Donec lectus leo
-                    </h2>
-                    <p className="dark:text-gray-800">
-                      Curabitur luctus erat nunc, sed ullamcorper erat
-                      vestibulum eget.
-                    </p>
-                  </div>
+              <div className="card rounded-sm card-compact bg-base-200">
+                <figure>
+                  <img
+                    src="https://i.ibb.co/0K1VLWk/2075.jpg"
+                    alt="Shoes"
+                    className="h-44 w-full"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">Enjoy the beauty of Spain</h2>
+                  <p>Toledo from hill in summer morning enjoy the sky view</p>
                 </div>
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className=" rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800">
-                <img
-                  src="https://source.unsplash.com/random/300x300/?2"
-                  alt=""
-                  className="object-cover object-center w-full rounded-t-md h-44 dark:bg-gray-500"
-                />
-                <div className="flex flex-col justify-between p-6 space-y-8">
-                  <div className="space-y-2">
-                    <h2 className="text-3xl font-semibold tracking-wide">
-                      Donec lectus leo
-                    </h2>
-                    <p className="dark:text-gray-800">
-                      Curabitur luctus erat nunc, sed ullamcorper erat
-                      vestibulum eget.
-                    </p>
-                  </div>
+              <div className="card rounded-sm card-compact bg-base-200">
+                <figure>
+                  <img
+                    src="https://i.ibb.co/ByZ3R5N/41940.jpg"
+                    alt="Shoes"
+                    className="h-44 w-full"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">Enjoy the beauty of Netherlands</h2>
+                  <p>Beautiful shot of the historic Binnenhof castle in the Netherlands</p>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="card rounded-sm card-compact bg-base-200">
+                <figure>
+                  <img
+                    src="https://i.ibb.co/YZ58cK8/427.jpg"
+                    alt="Shoes"
+                    className="h-44 w-full"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">Enjoy the beauty of Switzerland</h2>
+                  <p>Young woman hiker taking photo with smartphone on mountains peak in winter</p>
                 </div>
               </div>
             </SwiperSlide>
